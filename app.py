cfed4581be9acd452d186dd7740ef59a48ce13cf
Bishal <bishal67@git.in>
2026-01-03 21:23:26 +0600

    new

--- app.py
+++ app.py
@@ -219,5 +219,7 @@ def final_prediction():
     
     return jsonify(reply=text)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
